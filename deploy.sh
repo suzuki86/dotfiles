969cbe2563f5201808e6ff97d1e15f06229fbaa1
suzuki86 <tsnr0001@gmail.com>
2015-09-21 17:58:13 +0900

    Update clone command

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 mkdir -p ~/.vim/bundle
-git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
